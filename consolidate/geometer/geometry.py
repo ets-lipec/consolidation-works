843e337227fc3d56b7c2ff1b469c52d4583f3911
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-08-14 18:30:41 -0400

    Update geometry.py
    since we can have multiple domains, geometry is read for each of them

--- consolidate/geometer/geometry.py
+++ consolidate/geometer/geometry.py
@@ -9,17 +9,23 @@ Created on Sat Jul 25 11:38:36 2020
 class Geometry():
     def __init__(self, deck):
         self.deck = deck
+        self.n = int(self.deck.doc["Problem Type"]["Number of Domains"])
         self.geometry()
         
         
+        
     def geometry(self):
+        self.Keys = []
+        for i in range(self.n):
+            self.Keys.append("Domain " + str(i + 1))
+            
+            
+        self.Lx = {key: None for key in self.Keys}
+        self.Ly = {key: None for key in self.Keys}
+        for i in range(self.n):
+            Lx = float(self.deck.doc["Geometry"]["Length X"]["Domain " + str(i+1)])
+            Ly = float(self.deck.doc["Geometry"]["Length Y"]["Domain " + str(i+1)])
+            self.Lx[self.Keys[i]] = Lx
+            self.Ly[self.Keys[i]] = Ly
         
         
-        self.Lx=float(self.deck.doc["Geometry"]["Length X"])
-        
-        self.Ly = {
-  "Top Adherent": float(self.deck.doc["Geometry"]["Top Plate Thickness"]) ,
-  "HE": float(self.deck.doc["Geometry"]["HE"]),
-  "Bottom Adherent": float(self.deck.doc["Geometry"]["Bottom Plate Thickness"]) 
-                  }
-   
