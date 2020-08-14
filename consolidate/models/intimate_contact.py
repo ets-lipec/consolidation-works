0deab177a39798b06e1f811b43638d0e63dc0ed7
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-08-14 18:28:52 -0400

    Update intimate_contact.py
    This is the UNIQUE module that still needs to be "looping" over the domain... still thinking on how to do that

--- consolidate/models/intimate_contact.py
+++ consolidate/models/intimate_contact.py
@@ -9,47 +9,41 @@ import numpy as np
 
 class IntimateContact():
     
-    def __init__(self, meshes,deck):
-            self.meshes=meshes
-            self.deck=deck
-            self.P = (float(self.deck.doc["Boundary Condition"]["Consolidation Pressure"]))
-            self.init_parameter()
-            # self.calculate_average_dic()
-            
-            
-# -------------- BEGIN VISCOSITY CALCULATION ----------            
-            
+    def __init__(self, meshes, deck):
+        self.meshes = meshes
+        self.deck = deck
+        self.P = (float(self.deck.doc["Boundary Condition"]["Consolidation Pressure"]))
+        self.init_parameter()
+        # self.calculate_average_dic()
+        
+        
     def viscosity_timestep(self,v,u):
-             
-          v[0:, 0:]=1.14*10**(-12)*np.exp(26300/u[0:,0:])
-          return v
-
-
-# -------------- END TEMPERATURE INITIAL CONDITION ----------
-
-# -------------- BEGIN INTIMATE CONTACT CALCULATION----------     
-    def init_parameter(self):
-        self.aux=0
+        v[0:, 0:]=1.14*10**(-12)*np.exp(26300/u[0:,0:])
+        return v
     
+    
+    def init_parameter(self):
+        self.aux1 = 0
+        self.aux2 = 0
+        self.aux3 = 0
+        
+        
     def dic_timestep(self, dic, dic0, v, t):
         
         C1=5*(1+0.45)*(0.85)**2
-        # dic[(self.meshes.ny1 -1):(self.meshes.ny1 +1),0:]=dic0[(self.meshes.ny1 -1):(self.meshes.ny1 +1),0:]*(1+C1*self.P*10**6/v[(self.meshes.ny1 -1):(self.meshes.ny1 +1),0:]*(self.aux+t))**(1/5)
-        # dic[(self.meshes.ny.get("Bottom Adherent")):(self.meshes.ny.get("Bottom Adherent")+self.meshes.ny.get("HE")+1),0:]=dic0[(self.meshes.ny.get("Bottom Adherent")):(self.meshes.ny.get("Bottom Adherent")+self.meshes.ny.get("HE")+1),0:]*(1+C1*(self.aux+self.P*10**6/v[(self.meshes.ny.get("Bottom Adherent")):(self.meshes.ny.get("Bottom Adherent")+self.meshes.ny.get("HE")+1),0:]*t))**(1/5)
-        dic[self.meshes.ny.get("Bottom Adherent"):self.meshes.ny.get("Bottom Adherent")+self.meshes.ny.get("HE")+1,0:]=dic[self.meshes.ny.get("Bottom Adherent"):self.meshes.ny.get("Bottom Adherent")+self.meshes.ny.get("HE")+1,0:]*(1+C1*(self.aux+self.P*10**6/v[(self.meshes.ny.get("Bottom Adherent")):(self.meshes.ny.get("Bottom Adherent")+self.meshes.ny.get("HE")+1),0:]*t))**(1/5)
-        np.clip(dic,0,1)
-        return dic
-# -------------- END INTIMATE CONTACT CALCULATION----------     
-    
-    
-    def update_aux(self,t,v):
-        # self.aux=self.aux+t
-        # self.aux=self.aux+self.P*10**6/v[(self.meshes.ny1 -1):(self.meshes.ny1 +1),0:]*t
-        self.aux=self.aux+self.P*10**6/v[(self.meshes.ny.get("Bottom Adherent")):(self.meshes.ny.get("Bottom Adherent")+self.meshes.ny.get("HE")+1),0:]*t
         
-        return self.aux    
-    
-    
+        dic[self.meshes.ny["Domain 1"]-1, 0:] = dic[self.meshes.ny["Domain 1"]-1, 0:] * (1 + C1 * (self.aux1 + self.P*10**6/v[(self.meshes.ny["Domain 1"]-1), 0:]*t))**(1/5)
+        dic[self.meshes.ny["Domain 1"], 0:] = dic[self.meshes.ny["Domain 1"], 0:] * (1 + C1*(self.aux2 + self.P*10**6/v[(self.meshes.ny["Domain 1"]), 0:]*t))**(1/5)
+        dic[self.meshes.ny["Domain 1"] + self.meshes.ny["Domain 2"], 0:]=dic[self.meshes.ny["Domain 1"] + self.meshes.ny["Domain 2"], 0:]*(1+C1*(self.aux3 + self.P*10**6/v[(self.meshes.ny["Domain 1"] + self.meshes.ny["Domain 2"]), 0:]*t))**(1/5)
+        np.clip(dic, 0, 1)
+        
+        self.aux1=self.aux1+self.P*10**6/v[(self.meshes.ny["Domain 1"]-1), 0:]*t
+        self.aux2=self.aux2+self.P*10**6/v[(self.meshes.ny["Domain 1"]), 0:]*t
+        self.aux3=self.aux3+self.P*10**6/v[(self.meshes.ny["Domain 1"] + self.meshes.ny["Domain 2"]), 0:]*t
+        return dic
+
+
+
     # def calculate_average_dic(self):
     #     self.DicAverage=np.sum(self.meshes.Dic[int(self.meshes.ny/2-1):int(self.meshes.ny/2+1),1:-1])/((self.meshes.nx-2)*2)
     #     return self.DicAverage
